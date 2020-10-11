9024d135e5ae0c0aae6139e01f6b23f3eceb090d
Arka <arkajyotiadhikary15@gmail.com>
2020-10-11 18:40:35 +0530

    express simple rest

--- week1/3. node express/index.js
+++ week1/3. node express/index.js
@@ -1,14 +1,71 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
 
 
 const app = express();
 const port = 3000;
 const host = 'localhost';
 
+app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use(morgan('dev'));
+
+
+app.all('/dishes',(req,res,next)=>{
+    res.statusCode = 400;
+    res.setHeader('Content_Type','text/plain');
+    next();
+});
+
+// ___________________________________________________________________________________
+
+
+app.get('/dishes',(req,res,next)=>
+{
+    res.end('Will send all the dishes to you!');
+});
+
+app.post('/dishes',(req,res,next)=>
+{
+    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+});
+
+app.put('/dishes',(req,res,next)=>
+{
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /dishes');
+});
+
+app.delete('/dishes', (req, res, next) => {
+    res.end('Deleting all dishes');
+});
+
+// ___________________________________________________________________________________
+
+
+app.get('/dishes/:dishId', (req,res,next) => {
+    res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
+});
+
+app.post('/dishes/:dishId', (req, res, next) => {
+  res.statusCode = 403;
+  res.end('POST operation not supported on /dishes/'+ req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req, res, next) => {
+  res.write('Updating the dish: ' + req.params.dishId + '\n');
+  res.end('Will update the dish: ' + req.body.name + 
+        ' with details: ' + req.body.description);
+});
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
+
+// ___________________________________________________________________________________
 
 
 app.use((req,res,next)=>{
